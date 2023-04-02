a110292aac613d66745abdaed4461b4117c6f5c3
catcat4er <kerimov-t90@mail.ru>
2023-04-02 17:18:23 +0400

    added some annotations and personal data properties

--- src/test/java/com/gostechnadzor/TestWPO.java
+++ src/test/java/com/gostechnadzor/TestWPO.java
@@ -1,36 +1,46 @@
 package com.gostechnadzor;
 
 import com.gostechnadzor.insert_data.InsertData;
+import com.gostechnadzor.insert_data.InsertingMainData;
 import com.gostechnadzor.test_base.TestBase;
+import io.qameta.allure.Owner;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.sleep;
 
+@Owner("c@tcat4er")
+@DisplayName("Test lk GosTechNadzor")
 public class TestWPO extends TestBase {
 
     InsertData InsData = new InsertData();
+    InsertingMainData PersonalData = ConfigFactory.create(InsertingMainData.class);
+    String login = PersonalData.login();
+    String password = PersonalData.password();
+    String site_port = PersonalData.site_port();
+
 
     @Test
     public void openMainPagePositive(){
         InsData
-                .openBrowser(":23480")
-                .login("TEST", "Test1")
+                .openBrowser(site_port)
+                .login(login, password)
                 .assertTrueLogin();
     }
 
     @Test
     public void openMainPageNegative(){
         InsData
-                .openBrowser(":23480")
-                .login("TEST", "Text1")
+                .openBrowser(site_port)
+                .login(login, password)
                 .assertFalseLogin();
     }
 
     @Test
     public void testForm(){
         InsData
-                .openBrowser(":23480")
-                .login("TEST", "Test1")
+                .openBrowser(site_port)
+                .login(login, password)
                 .assertTrueLogin()
                 .addIndividual()
                 .addName("Имя")

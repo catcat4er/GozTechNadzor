c2f17428a8102408eee14b8d8a3d875ee4cacc85
catcat4er <kerimov-t90@mail.ru>
2023-04-02 17:18:33 +0400

    added some annotations and personal data properties

---
+++ src/test/java/com/gostechnadzor/insert_data/InsertingMainData.java
@@ -0,0 +1,11 @@
+package com.gostechnadzor.insert_data;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:Secret_data.properties")
+public interface InsertingMainData extends Config {
+    String login();
+    String password();
+    String site_port();
+
+}
